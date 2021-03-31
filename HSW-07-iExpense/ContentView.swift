7fada5108bf015509a080bc82557a20e40317239
Manuel Casocavallo <manuelcasocavallo@gmail.com>
2021-03-31 16:13:35 +0200

    Initial Commit

---
+++ HSW-07-iExpense/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HSW-07-iExpense
+//
+//  Created by Manuel Casocavallo on 31/03/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HSW-07-iExpense/HSW_07_iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  HSW_07_iExpenseApp.swift
+//  HSW-07-iExpense
+//
+//  Created by Manuel Casocavallo on 31/03/21.
+//
+
+import SwiftUI
+
+@main
+struct HSW_07_iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
